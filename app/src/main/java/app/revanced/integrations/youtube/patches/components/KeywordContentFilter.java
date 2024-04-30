51657062df01b17f7e36bd9455042af9e827bd19
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-05-01 02:07:01 +0900

    fix(YouTube/Hide feed components): filter shorts found in horizontal shelves

--- app/src/main/java/app/revanced/integrations/youtube/patches/components/KeywordContentFilter.java
+++ app/src/main/java/app/revanced/integrations/youtube/patches/components/KeywordContentFilter.java
@@ -97,7 +97,8 @@ public final class KeywordContentFilter extends Filter {
     private final StringFilterGroup containsFilter = new StringFilterGroup(
             null,
             "modern_type_shelf_header_content.eml",
-             "shorts_lockup_cell.eml" // Part of 'shorts_shelf_carousel.eml'
+            "shorts_lockup_cell.eml", // Part of 'shorts_shelf_carousel.eml'
+            "video_card.eml" // Shorts that appear in a horizontal shelf.
     );
 
     private final StringFilterGroup commentsFilter;
@@ -161,7 +162,7 @@ public final class KeywordContentFilter extends Filter {
             return sentence;
         }
         final int firstCodePoint = sentence.codePointAt(0);
-        // In some non English languages title case is different than upper case.
+        // In some non English languages title case is different than uppercase.
         return new StringBuilder()
                 .appendCodePoint(Character.toTitleCase(firstCodePoint))
                 .append(sentence, Character.charCount(firstCodePoint), sentence.length())
@@ -175,6 +176,7 @@ public final class KeywordContentFilter extends Filter {
         if (sentence.isEmpty()) {
             return sentence;
         }
+
         final int delimiter = ' ';
         // Use code points and not characters to handle unicode surrogates.
         int[] codePoints = sentence.codePoints().toArray();
